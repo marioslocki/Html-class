3ed569ae69cd6e67949553b7d865647aeebd1d50
marioslocki <116668026+marioslocki@users.noreply.github.com>
2023-04-04 05:02:39 +0300

    Add files via upload

---
+++ New folder/javascript.js
@@ -0,0 +1,73 @@
+
+$(function() {
+    $('#rating').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating1').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating2').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating3').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating4').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating5').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
+  $(function() {
+    $('#rating').raty({
+      path: 'https://cdnjs.cloudflare.com/ajax/libs/raty/2.9.0/images/',
+      score: 3,
+      click: function(score, evt) {
+        alert('You rated ' + score + ' stars!');
+      }
+    });
+  });
